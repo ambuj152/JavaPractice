b458b313bd95be74332e4ab0ab0a78c009d0f5e8
ambuj152 <ambuj152@gmail.com>
2023-12-17 18:08:11 +0530

    prodesk test

--- duplicate.java
+++ duplicate.java
@@ -1,27 +1,21 @@
-class Duplicate{
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-   int Student[]={3,9,18,3,28,6,17,9,3} ;
-   for(int i=0;i<Student.length;i++) 
-   {
-    for(int j=i+1;i<Student.length;j++)
-    
-     if( Student[i]==Student[j])
-      
-     {  
-         System.out.println(Student[j]);
-        
-         Student[i]=Student[i+1];
-         Student[j]=Student[j+1];
-
+class Duplicate
+{
+   
+    public static void main(String[] args)
+    {
+         int Student[]={3,9,18,3,28,6,17,9,3} ;
+         for(int i=0;i<Student.length;i++) 
+            {
+             for(int j=i+1;i<Student.length;j++)
+                   if( Student[i]==Student[j])
+                      {  
+                        System.out.println(Student[j]);
+                        Student[i]=Student[i+1];
+                        Student[j]=Student[j+1];      
          
-        
-     
-     }
+                      }
       
-   }      
+            }      
 
     }
 }
