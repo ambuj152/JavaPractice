bda1b00827ad2f9c3a2277c4280bb1e3472e078c
ambuj152 <ambuj152@gmail.com>
2023-12-23 03:54:33 +0530

    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.

--- duplicate.java
+++ duplicate.java
@@ -3,16 +3,23 @@ class Duplicate
    
     public static void main(String[] args)
     {
-         int Student[]={3,9,18,3,28,6,17,9,3} ;
+         int Student[]={3,9,18,3,28,6,17,9,3,6} ;
          for(int i=0;i<Student.length;i++) 
             {
              for(int j=i+1;i<Student.length;j++)
                    if( Student[i]==Student[j])
                       {  
                         System.out.println(Student[j]);
-                        Student[i]=Student[i+1];
-                        Student[j]=Student[j+1];      
+                        Student[j]=Student[j+1]; 
+                        Student[i]=Student[i+1]; 
+                     
+                            
          
+                      }
+                      else
+                      {
+                       System.out.println("error");
+                      
                       }
       
             }      
---
+++ target.java
@@ -0,0 +1,21 @@
+class target{
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        int target=5;
+        int num= arr.length-1;
+        for(int i=0;i<num;i++)
+        {
+            for(int j=i+1;j<num;j++)
+            {
+                if(arr[i]+arr[j]==target)
+                {
+                    System.out.print(arr[i]);
+                     System.out.print(arr[j]);
+
+                     
+                     arr[j]=arr[j+1];  
+                }arr[i]=arr[i+1];
+            }
+        }
+    }
+}
