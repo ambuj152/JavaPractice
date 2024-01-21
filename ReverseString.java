8932947d08af282724e1eaeada346c5d1e679325
ambuj152 <ambuj152@gmail.com>
2024-01-21 21:28:12 +0530

    reverse string

---
+++ ReverseString.java
@@ -0,0 +1,20 @@
+public class ReverseString {
+       
+        public static void main(String[] args) 
+        {
+         String originalString = "Ambuj yadav";
+            String reversedString = reverseString(originalString);
+             System.out.println("Original String: " + originalString);
+              System.out.println("Reversed String: " + reversedString);
+        }
+        public static String reverseString(String input) 
+        {
+            StringBuilder stringBuilder = new StringBuilder(input);
+            stringBuilder.reverse();
+            return stringBuilder.toString();
+        }
+    }
+    
+
+
+
