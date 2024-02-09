d1b163bdea3d9b983e27e99a7032e71db0b853bd
Ambuj yadav <57595847+ambuj152@users.noreply.github.com>
2024-02-09 17:52:14 +0530

    Update target.java

--- target.java
+++ target.java
@@ -2,7 +2,8 @@
 return indices of the two numbers such that they add up to target.*/
 
 class target{
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         int arr[]={1,2,3,4,5};
         int target=5;
         int num= arr.length-1;
@@ -13,10 +14,9 @@ class target{
                 if(arr[i]+arr[j]==target)
                 {
                     System.out.print(arr[i]+",");
-                     System.out.print(arr[j]);
+                    System.out.print(arr[j]);
                      arr[j]=arr[j+1];  
                 }arr[i]=arr[i+1];
-            }
-        }
-    }
-}
+             }
+             }
+              }}
